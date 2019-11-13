1c861c3e276039cc86ea548d720d1f51be69c48e
Shorn Jacob <shorn@transparency.consulting>
2019-11-13 16:12:12 +1300

    Simple Form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//https://github.com/dabit3/aws-amplify-workshop-react#custom-authentication-strategies
 
-function App() {
+import React, { useReducer } from "react";
+
+export default function App() {
+  // define initial state
+  const initialState = {
+    email: "",
+    password: ""
+  };
+
+  // create reducer
+  function reducer(state, action) {
+    switch (action.type) {
+      case "SET_INPUT":
+        return { ...state, [action.inputName]: action.inputValue };
+      default:
+        return state;
+    }
+
+    
+  }
+
+  // useReducer hook creates local state
+  //array destructuring
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // event handler
+  function onChange(e) {
+    dispatch({
+      type: "SET_INPUT",
+      inputName: e.target.name,
+      inputValue: e.target.value
+    });
+
+    // console.log(state)
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        name="email"
+        placeholder="email"
+        value={state.username}
+        onChange={onChange}
+      />
+
+      <input
+        name="password"
+        placeholder="password"
+        value={state.username}
+        onChange={onChange}
+      />
     </div>
   );
 }
-
-export default App;
