b174cf0b7f846688d2a95eb747ac1ac0fbbf3584
Shorn Jacob <shorn@transparency.consulting>
2019-11-20 15:40:45 +1300

    Text Field Working with react-hook-form

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Amplify from 'aws-amplify'
 import App from './auth';
+import App2 from './App2';
 import config from './aws-exports'
 Amplify.configure(config)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App2 />, document.getElementById('root'));
 
