8ebef921d0b61f9293210a644b0684361ef88076
Shorn Jacob <shorn@transparency.consulting>
2020-01-13 22:07:12 +1300

    Converted appbar to menuItems

--- src/auth/components/simpleAppBar.js
+++ src/auth/components/simpleAppBar.js
@@ -4,10 +4,13 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { navigate } from "@reach/router";
 import { logout } from "services/auth";
+import IconButton from '@material-ui/core/IconButton';
+import MenuOpenIcon from '@material-ui/icons/MenuOpen';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
 //https://reactjs.org/docs/test-utils.html#isdomcomponent
 
@@ -23,6 +26,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+//https://github.com/mui-org/material-ui/blob/master/docs/src/pages/components/app-bar/MenuAppBar.js
 export default function SimpleAppBar({
   authenticated,
   authenticating,
@@ -33,52 +37,52 @@ export default function SimpleAppBar({
   // console.log(authenticated)
   // console.log(email)
 
-  let links;
-  if (authenticated) {
-    links = (
-      <React.Fragment>
-        <Button color="inherit" onClick={() => navigate("/profile")}>
-          Profile
-        </Button>
-        {/* callback needs to execute two statements, navigate and changestate */}
-        <Button
-          color="inherit"
-          onClick={() =>
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  // const handleChange = event => {
+  //   setAuth(event.target.checked);
+  // };
+
+  const handleMenu = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
+  let menuItems
+
+  //Unauthenticated Menu
+  let unauthMenu = (
+    <div>
+          <MenuItem onClick={() => navigate("/signin")}>Sign In</MenuItem>
+          <MenuItem onClick={() => navigate("/signup")}>Sign Up</MenuItem>
+          <MenuItem onClick={() =>  navigate("/confirmsignup")}>Confirm Sign Up</MenuItem>
+          <MenuItem onClick={() => navigate("/forgotpassword")}>Request Password Reset</MenuItem>
+          <MenuItem onClick={() => navigate("/forgotpasswordsubmit")}>Reset Password</MenuItem>
+    </div>
+  )
+
+  let authMenu = (
+    <div>
+          <MenuItem onClick={() => navigate("/profile")}>Profile</MenuItem>
+          <MenuItem onClick={() =>
             logout(() => {
               logoutSuccess()
               navigate("/signin");
             })
-          }
-        >
-          SignOut
-        </Button>
-      </React.Fragment>
-    );
-  } else {
-    links = (
-      <React.Fragment>
-        <Button color="inherit" onClick={() => navigate("/signin")}>
-          SignIn
-        </Button>
-        <Button color="inherit" onClick={() => navigate("/signup")}>
-          SignUp
-        </Button>
-        <Button color="inherit" onClick={() => navigate("/confirmsignup")}>
-          ConfirmSignUp
-        </Button>
-        <Button color="inherit" onClick={() => navigate("/forgotpassword")}>
-          ForgotPassword
-        </Button>
-        <Button
-          color="inherit"
-          onClick={() => navigate("/forgotpasswordsubmit")}
-        >
-          ForgotPasswordSubmit
-        </Button>
-      </React.Fragment>
-    );
-  }
+          }>Sign Out</MenuItem>
+        
+    </div>
+  )
+  
+  menuItems = authenticated ? authMenu : unauthMenu
+      
 
+  // console.log(menuItems)
   return (
     <div className={classes.root}>
       <AppBar position="static">
@@ -87,7 +91,39 @@ export default function SimpleAppBar({
             {authenticated ? email : "Transparency Consulting"}
           </Typography>
           {/* https://reactjs.org/docs/conditional-rendering.html       */}
-          {links}
+          {/* {links} */}
+
+          <div>
+              <IconButton
+                aria-label="account menu"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <MenuOpenIcon />
+              </IconButton>
+              
+          
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                {menuItems}
+              </Menu>
+            </div>
+
         </Toolbar>
       </AppBar>
     </div>
