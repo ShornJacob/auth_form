ea013e30bfe9b2542c93ed601bb0bafbf117863c
Shorn Jacob <shorn@transparency.consulting>
2020-01-14 12:32:02 +1300

    Description for Menu in appbar -simpleappbar

--- src/auth/components/simpleAppBar.js
+++ src/auth/components/simpleAppBar.js
@@ -39,12 +39,13 @@ export default function SimpleAppBar({
 
 
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
+
 
   // const handleChange = event => {
   //   setAuth(event.target.checked);
   // };
 
+  //onclicking the iconbutton, the anchorEl of menu is set to the IconButton
   const handleMenu = event => {
     setAnchorEl(event.currentTarget);
   };
@@ -109,15 +110,15 @@ export default function SimpleAppBar({
                 id="menu-appbar"
                 anchorEl={anchorEl}
                 anchorOrigin={{
-                  vertical: 'top',
+                  vertical: 'bottom',
                   horizontal: 'right',
                 }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={open}
+                // keepMounted
+                // transformOrigin={{
+                //   vertical: 'top',
+                //   horizontal: 'right',
+                // }}
+                open={Boolean(anchorEl)}
                 onClose={handleClose}
               >
                 {menuItems}
@@ -129,3 +130,25 @@ export default function SimpleAppBar({
     </div>
   );
 }
+
+//https://material-ui.com/components/menus/
+//https://material-ui.com/api/menu/
+//anchorEl The DOM element used to set the position of the menu.
+
+//open *	bool		If true, the menu is visible.
+//open is  a Boolean of anchor element
+//onClose	func		Callback fired when the component requests to be closed.
+
+//he Menu component uses the Popover component internally. 
+//Popover has anchorOrigin
+
+
+//Modal is a lower-level construct that is leveraged byPopover
+//keepMounted	bool	false	Always keep the children in the DOM. This prop can be useful in SEO situation or when you want to maximize the responsiveness of the Modal.
+
+//https://material-ui.com/components/buttons/
+//con buttons are commonly found in app bars and toolbars.
+
+//https://stackoverflow.com/questions/40167287/react-material-ui-how-do-i-know-i-can-use-onclick-for-button
+//https://reactjs.org/docs/events.html#mouse-events
+//onClick is from react
